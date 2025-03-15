feb0f81d17c3d63fcef087c278848b72d30b5ef7
PPPcar <43994129+PPPcar@users.noreply.github.com>
2025-03-15 19:48:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ from zhdate import ZhDate
 global false, null, true
 false = null = true = ''
 def get_color():
-    # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list = get_colors(100)
-    return random.choice(color_list)
+    # 生成大写 + 固定 # 前缀
+    return "#%06X" % random.randint(0, 0xFFFFFF)
 
 
 def get_access_token():
